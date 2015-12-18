9012e777045a0b463be23107a784339ad0c5e560
Ronak <thackerronak@gmail.com>
2015-12-18 09:58:23 +0530

    splash screen and animation add

--- app/src/main/java/com/smartsense/gifkar/SplashActivity.java
+++ app/src/main/java/com/smartsense/gifkar/SplashActivity.java
@@ -1,28 +1,63 @@
 package com.smartsense.gifkar;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.smartsense.gifkar.utill.CommonUtil;
 import com.smartsense.gifkar.utill.LocationSettingsHelper;
 
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements AnimationListener {
     private LocationSettingsHelper mSettingsHelper;
-
+    ImageView img1, img2, img3, img4, img5;
+    private Animation an1,an2,an3,an4,an5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        img1=(ImageView) findViewById(R.id.imageView);
+        img2=(ImageView) findViewById(R.id.imageView2);
+        img3=(ImageView) findViewById(R.id.imageView3);
+        img4=(ImageView) findViewById(R.id.imageView4);
+        img5=(ImageView) findViewById(R.id.imageView5);
+        an1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left);
+        img1.startAnimation(an1);
+        an2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom);
+        an3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom);
+        an4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom);
+        an5 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom);
 //        if (CommonUtil.isGPS(getApplicationContext())) {
-        startApp();
+//        startApp();
 //        } else {
 //            mSettingsHelper = new LocationSettingsHelper(this);
 //            mSettingsHelper.checkSettings();
 //        }
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // TODO Auto-generated method stub
+        if (animation == an1) {
+            img2.setVisibility(View.VISIBLE);
+            img2.startAnimation(an2);
+        } else if (animation == an2) {
+            img3.setVisibility(View.VISIBLE);
+            img3.startAnimation(an3);
+        } else if (animation == an3) {
+            img4.setVisibility(View.VISIBLE);
+            img4.startAnimation(an4);
+        } else if (animation == an4) {
+            img5.setVisibility(View.VISIBLE);
+            img5.startAnimation(an5);
+        }
 
     }
 
@@ -69,6 +104,19 @@ public class SplashActivity extends AppCompatActivity {
                 break;
 
         }
+    }
+
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
+
     }
 
 }
